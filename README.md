69dbe843ddd2b12d41b7ad9e01b6031a43e5593f
Filip Kucera <kucerafilip@gmail.com>
2017-02-04 12:38:27 +0100

    blink working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# raspberry-ledcontrol
+# Raspberry Pi ledcontrol
 pi4j project to blink LED on raspberry pi
+
+##Usage
+* connect LED to GPIO 0 on **Raspberry Pi 3 Model B** 
+(according to GPIO numbering schema http://pi4j.com/images/j8header-3b-large.png)
+* build project by maven `mvn install`
+* copy ledcontroll-1.0-SNAPSHOT-jar-with-dependencies.jar from target 
+to you raspberry
+* On raspberry, run _`sudo java -jar ledcontroll-1.0-SNAPSHOT-jar-with-dependencies.jar`_
+ (all dependencies on com.pi4j.io.gpio library a packaged in build jar)
--- src/test/java/cz/fku/raspberrypi/ledcontrol/ControlGpioAppTest.java
+++ src/test/java/cz/fku/raspberrypi/ledcontrol/ControlGpioAppTest.java
@@ -1,13 +1,15 @@
-package pi-ledcontroll;
+package cz.fku.raspberrypi.ledcontrol;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.InterruptedIOException;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class ControlGpioAppTest
     extends TestCase
 {
     /**
@@ -15,7 +17,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public ControlGpioAppTest(String testName )
     {
         super( testName );
     }
@@ -25,14 +27,14 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( ControlGpioAppTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testApp() throws InterruptedException
     {
-        assertTrue( true );
+        ControlGpioApp.main(null);
     }
 }
