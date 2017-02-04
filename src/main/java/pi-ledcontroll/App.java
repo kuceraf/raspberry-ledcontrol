e6644312522d6f3b449876ea7f416b0591c3988b
Filip Kucera <kucerafilip@gmail.com>
2017-02-04 11:17:39 +0100

    maven project generated

---
+++ src/main/java/pi-ledcontroll/App.java
@@ -0,0 +1,13 @@
+package pi-ledcontroll;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
